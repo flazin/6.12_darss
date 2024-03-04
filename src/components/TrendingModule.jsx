2f49741cd608306dee0fcdb13469bf6766d92bcd
akmaljon <akmaljon@gmail.com>
2024-03-04 09:07:37 +0500

    efsdzx

--- src/components/TrendingModule.jsx
+++ src/components/TrendingModule.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import styled from 'styled-components';
-import MediaInstance from './MediaInstance';
-import ScrollContainer from 'react-indiana-drag-scroll';
-import 'animate.css';
+import React from "react";
+import styled from "styled-components";
+import MediaInstance from "./MediaInstance";
+import ScrollContainer from "react-indiana-drag-scroll";
+import "animate.css";
 
 const StyledTrendingModule = styled.section`
   h1 {
@@ -14,24 +14,24 @@ const StyledTrendingModule = styled.section`
     white-space: nowrap;
     overflow: auto;
     /* ===== Scrollbar CSS ===== */
-  /* Firefox */
+    /* Firefox */
     scrollbar-width: 4px;
     scrollbar-color: #5a698f #161d2f;
 
-  /* Chrome, Edge, and Safari */
-  &::-webkit-scrollbar {
-    height: 4px;
-  }
+    /* Chrome, Edge, and Safari */
+    &::-webkit-scrollbar {
+      height: 4px;
+    }
 
-  &::-webkit-scrollbar-track {
-    background: #161d2f;
-  }
+    &::-webkit-scrollbar-track {
+      background: #161d2f;
+    }
 
-  &::-webkit-scrollbar-thumb {
-    background-color: #5a698f;
-    border-radius: 10px;
-    border: 3px none #ffffff;
-  }
+    &::-webkit-scrollbar-thumb {
+      background-color: #5a698f;
+      border-radius: 10px;
+      border: 3px none #ffffff;
+    }
 
     > button {
       display: inline-block;
@@ -41,20 +41,29 @@ const StyledTrendingModule = styled.section`
       margin-left: 1rem;
     }
   }
-`
+`;
 
 const TrendingModule = ({ name, contentList, screenSize, setContentList }) => {
-
   return (
-    <StyledTrendingModule className={['animate__animated', 'animate__fadeInDown']}>
+    <StyledTrendingModule
+      className={["animate__animated", "animate__fadeInDown"]}
+    >
       <h1>{name}</h1>
       <ul>
-        <ScrollContainer className='scroll-container' hideScrollbars={false}>
-          {contentList.map((media) => <MediaInstance mediaData={media} key={media.title} isTrending={true} screenSize={screenSize} setContentList={setContentList} />)}
+        <ScrollContainer className="scroll-container" hideScrollbars={true}>
+          {contentList.map((media) => (
+            <MediaInstance
+              mediaData={media}
+              key={media.title}
+              isTrending={true}
+              screenSize={screenSize}
+              setContentList={setContentList}
+            />
+          ))}
         </ScrollContainer>
       </ul>
     </StyledTrendingModule>
-  )
-}
+  );
+};
 
-export default TrendingModule
+export default TrendingModule;
